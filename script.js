7bfd1befbc988165b47c3bb624e59d5dbc2cf9da
Nick <Nick_II2000@mail.ru>
2022-05-12 18:59:49 +0600

    3-th commit

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ btn[1].onclick = () => {
     alert("WOW-2");
 };
 
-btn[2].addEventListener('click', () => {
-    alert('WOW-3');
+// btn[2].addEventListener('click', () => {
+//     alert('WOW-3');
+// });
+
+// btn[2].addEventListener('click', () => {
+//     alert('Second click');
+// });
+
+btn[2].addEventListener('mouseenter', () => {
+    console.log('Hover');
 });
